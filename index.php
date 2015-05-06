750582b2151fbb228e43eaf7505c94286aa3d89c
watchthemoon <anneliesvandebroek@hotmail.com>
2015-05-06 16:50:09 +0200

    Begin website

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>T.A.G.</title>
+<link href="assets/css/stylesheet.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+
+<script src="assets/js/jquery-1.11.0.min.js"></script>
+<script src="assets/js/lightbox.min.js"></script>
+<link href="assets/css/lightbox.css" rel="stylesheet" />
+</head>
+
+<body>
+
+
+<div class="website">
+<header>
+	<div class="head">
+		
+	</div>
+</header>
+<section class="sectionmenu">
+	<div class="headmenu">
+	<a href="index.html"><img src="" alt="TAG"></a>
+		<nav>
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="categories.html">Overzicht</a></li>
+			<li><a href="aanmelden.html">Over T.A.G.</a></li>
+		</ul>
+		</nav>
+	</div>
+</section>
+<section class="contain">
+<div class="intro">
+</div>
+<div class="content">
+	<div class="button">
+		Fruit
+	</div>
+	<div class="button">
+		Groenten
+	</div>
+	<div class="button2">
+		Producten
+	</div>
+</div>
+
+</section>
+<div class="push"></div>
+</div>
+<footer>
+</footer>
+</body>
+
+</html>
